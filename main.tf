6ebd89e922139dea28c0a5f54d3d236fc7c687f4
Alfred-Sepulveda <006372934@coyote.csusb.edu>
2022-09-25 09:26:50 -0700

    exposed

--- main.tf
+++ main.tf
@@ -1,36 +1,21 @@
-provider "aws"{
-	region = "us-east-1"
-	access_key = "AKIASTCVFDZ35E5YAB4A"
-	secret_key = "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"
-}
+provider "aws"{}
 
-resource "aws_vpc" "development-vpc"{
-    cidr_block = "10.0.0.0/16"
+variable cidr_blocks{}
+variable avail_zone{}
+
+resource "aws_vpc" "myapp-vpc"{
+    cidr_block = var.cidr_blocks[0].cidr_block
     tags = {
-        Name: "development"
-        vpc_env: "dev"
+        Name: var.cidr_blocks[0].name
     }
 }
 
 resource "aws_subnet" "dev-subnet-1"{
     vpc_id = aws_vpc.development-vpc.id
-    cidr_block = "10.0.10.0/24" 
-    availability_zone = "us-east-1a"
+    cidr_block = var.cidr_blocks[1].cidr_block 
+    availability_zone = var.avail_zone
     tags = {
-        Name: "subnet-1-dev"
+        Name: var.cidr_blocks[1].name
     } 
-}
-
-data "aws_vpc" "existing_vpc"{
-    default = true
-}
-
-resource "aws_subnet" "dev-subnet-2"{
-    vpc_id = data.aws_vpc.existing_vpc.id
-    cidr_block = "172.31.48.0/20"
-    availability_zone = "us-east-1a"
-    tags = {
-        Name: "subnet-2-dev"
-    }
 }
 
