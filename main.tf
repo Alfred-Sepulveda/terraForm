e6e1ddf77a008586bb0de3cd51ff853915af835d
Alfred-Sepulveda <006372934@coyote.csusb.edu>
2022-09-25 08:54:05 -0700

    update

--- main.tf
+++ main.tf
@@ -1,27 +1,36 @@
-provider "aws"{}
-
-variable "cidr_block" {
-    description = "cidr_block, name, vpc,  and subnet"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
+provider "aws"{
+	region = "us-east-1"
+	access_key = "AKIASTCVFDZ35E5YAB4A"
+	secret_key = "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"
 }
 
-variable avail_zone {}
-
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_block[0].cidr_block
+resource "aws_vpc" "development-vpc"{
+    cidr_block = "10.0.0.0/16"
     tags = {
-        Name: var.cidr_block[0].name
+        Name: "development"
+        vpc_env: "dev"
     }
 }
- 
-resource "aws_subnet" "dev-subnet-1" {
+
+resource "aws_subnet" "dev-subnet-1"{
     vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_block[1].cidr_block
-    availability_zone = var.avail_zone
+    cidr_block = "10.0.10.0/24" 
+    availability_zone = "us-east-1a"
     tags = {
-        Name: var.cidr_block[1].name
+        Name: "subnet-1-dev"
+    } 
+}
+
+data "aws_vpc" "existing_vpc"{
+    default = true
+}
+
+resource "aws_subnet" "dev-subnet-2"{
+    vpc_id = data.aws_vpc.existing_vpc.id
+    cidr_block = "172.31.48.0/20"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name: "subnet-2-dev"
     }
 }
+
